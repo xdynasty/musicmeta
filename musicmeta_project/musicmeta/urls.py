ff9d7f84f450c7a966875f7f8a723bab9cf9a670
xdynasty <xdynasty5201314@gmail.com>
2020-11-19 23:09:57 -0500

    add releast-group path

--- musicmeta_project/musicmeta/urls.py
+++ musicmeta_project/musicmeta/urls.py
@@ -5,4 +5,5 @@ from . import views
 app_name = 'musicmeta'
 urlpatterns = [
     path('', views.index, name="index"),
+    path('artist/<str:artist_id>/<int:page>/', views.artist, name="artist")
 ]
--- musicmeta_project/musicmeta/views.py
+++ musicmeta_project/musicmeta/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from django.template.defaulttags import register
 from musicbrainzngs import *
+import math
 
 # Create your views here.
 
@@ -11,12 +12,64 @@ def get_begin(dictionary):
   return dictionary.get('life-span').get('begin')
 
 
+@register.filter
+def get_release_date(dictionary):
+  return dictionary.get('first-release-date')
+
+
 def index(request):
   query = request.GET.get('searchInput', False)
   if query:
     set_format('json')
-    result = search_artists(query)
-    print(type(result))
-    for artist in result["artists"]:
-      print(artist)
-  return render(request, 'musicmeta/index.html', {'artists': result["artists"]})
+    result = search_artists(query, strict=True)
+    return render(request, 'musicmeta/index.html', {'artists': result["artists"]})
+  else:
+    return render(request, 'musicmeta/index.html')
+
+
+def artist(request, artist_id, page):
+  result = browse_release_groups(artist_id, limit=100, offset=page*100)
+  count = result['release-group-count']
+  pages = math.ceil(count / 100)
+
+  albums, album_compilations, album_lives, singles, single_lives, single_remixes, single_soundtracks, eps, ep_lives, broadcasts, broadcast_lives, others = [
+  ], [], [], [], [], [], [], [], [], [], [], []
+  for release in result["release-group-list"]:
+    if 'secondary-type' not in release and release['primary-type'] == 'Album':
+      albums.append(release)
+    elif 'secondary-type' not in release and release['primary-type'] == 'Single':
+      singles.append(release)
+    elif 'secondary-type' not in release and release['primary-type'] == 'EP':
+      eps.append(release)
+    elif 'secondary-type' not in release and release['primary-type'] == 'Broadcast':
+      broadcasts.append(release)
+    elif release['primary-type'] == 'Album' and release['secondary-type'] == 'Compilation':
+      album_compilations.append(release)
+    elif release['primary-type'] == 'Album' and release['secondary-type'] == 'Live':
+      album_lives.append(release)
+    elif release['primary-type'] == 'Single' and release['secondary-type'] == 'Live':
+      single_lives.append(release)
+    elif release['primary-type'] == 'Single' and release['secondary-type'] == 'Remix':
+      single_remixes.append(release)
+    elif release['primary-type'] == 'Single' and release['secondary-type'] == 'Soundtrack':
+      single_soundtracks.append(release)
+    elif release['primary-type'] == 'EP' and release['secondary-type'] == 'Live':
+      ep_lives.append(release)
+    elif release['primary-type'] == 'Broadcast' and release['secondary-type'] == 'Live':
+      broadcast_lives.append(release)
+    else:
+      others.append(release)
+  albums.sort(key=lambda x: x['first-release-date'])
+  album_compilations.sort(key=lambda x: x['first-release-date'])
+  album_lives.sort(key=lambda x: x['first-release-date'])
+  singles.sort(key=lambda x: x['first-release-date'])
+  single_lives.sort(key=lambda x: x['first-release-date'])
+  single_remixes.sort(key=lambda x: x['first-release-date'])
+  single_soundtracks.sort(key=lambda x: x['first-release-date'])
+  eps.sort(key=lambda x: x['first-release-date'])
+  ep_lives.sort(key=lambda x: x['first-release-date'])
+  broadcasts.sort(key=lambda x: x['first-release-date'])
+  broadcast_lives.sort(key=lambda x: x['first-release-date'])
+  others.sort(key=lambda x: x['first-release-date'])
+  return render(request, 'musicmeta/artist.html', {"pages": range(pages), "page": page, "artist_id": artist_id, "albums": albums, "album_compilations": album_compilations, "album_lives": album_lives, "singles": singles, "single_lives": single_lives, "single_remixes": single_remixes, "single_soundtracks": single_soundtracks, "eps": eps, "ep_lives": ep_lives, "broadcasts": broadcasts, "broadcast_lives": broadcast_lives, "others": others})
+  return HttpResponse("You are at artists")
